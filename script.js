c9b7e08e4a53e7478880929492d0553e2a3cf417
RobinHerzig <robin.hrzg@gmail.com>
2022-09-14 19:44:26 -0500

    Addiontal styling and commenting

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+// Select rating to change color and reassign value to a variable
+
 let ratingNumber = document.querySelectorAll('.ratingNumber')
-Array.from(ratingNumber).forEach(elem => elem.addEventListener('click', changeColor));
+Array.from(ratingNumber).forEach(elem => elem.addEventListener('click', selectRating));
+
 let rating = 0
 
-function changeColor() {
+function selectRating() {
   if (this.classList.contains('selectedRating')) {
+    document.querySelector('#submitRating').classList.add('inactive')
     ratingNumber.forEach(elem => elem.classList.remove('selectedRating'))
     rating = 0
   }
   else {
+    document.querySelector('#submitRating').classList.remove('inactive')
     ratingNumber.forEach(elem => elem.classList.remove('selectedRating'))
     this.classList.add('selectedRating')
     rating = this.innerHTML
   }
-  console.log(rating)
 }
+
+// Submit the rating
 
 document.querySelector('#submitRating').addEventListener('click', submitRating)
 
